0f4b88db8085f059c0ff1ec5fbbca7cb1f21e1bc
Brad Vorjohan <brad@bythepeopleapp.com>
2020-05-20 09:50:21 -0400

    initial commit

---
+++ background.js
@@ -0,0 +1,3 @@
+// I run in the background, can persist for the duration of the browser session,
+// and am useful for storing state, listening for browser events, etc.
+// this is where you would store, update, load, etc. your personal settings. 
---
+++ content.js
@@ -0,0 +1,9 @@
+// this file runs on page load, and can interact with the DOM
+
+
+// test to be sure is working an filter domains correctly
+alert("Hello")
+
+
+// TODO: only begin when meeting starts (not on page load)
+// TODO: figure out muting people
