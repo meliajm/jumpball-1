5ac2538ce50364d6a938b610002d937b08b2914d
github username <meliajm@gmail.com>
2020-06-19 11:50:12 -0500

    code review?

--- content.js
+++ content.js
@@ -1,8 +1,63 @@
+// do while loop?
+// break or continue?
 let youAreMutedBool 
 
 // sometimes this array includes two fasle with only one other user, not sure why, maybe have to do with 
 // which mutations are being observed
 let othersAreMutedArr
+
+const attachJumpballText = () => {
+    // const toAttachJumpballText = document.querySelector(".knW4sf.OvyUed")
+    //  koV58.Zi94Db.S7urwe
+    const toAttachJumpballText = document.querySelector(".pHsCke")
+    toAttachJumpballText.insertAdjacentHTML('afterend', drawJumpballText())
+
+}
+
+const drawJumpballText = () => {
+    return `<div class="jumpball-text">
+    <svg width="520" height="120">
+
+  <rect x="0" y="0" width="100" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+  <rect x="50" y="0" width="10" height="80" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+  <rect x="0" y="90" width="40" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+ 
+  <rect x="110" y="0" width="10" height="80" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="140" y="0" width="10" height="80" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="110" y="90" width="40" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   
+   <rect x="170" y="0" width="10" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="190" y="20" width="10" height="30" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="210" y="0" width="10" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   
+   <rect x="230" y="0" width="10" height="80" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="230" y="90" width="10" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="240" y="0" width="30" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="240" y="30" width="30" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="270" y="0" width="10" height="40" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   
+   <rect x="290" y="0" width="10" height="90" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="290" y="0" width="30" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   
+   <rect x="320" y="0" width="10" height="30" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="290" y="30" width="30" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="300" y="80" width="30" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="330" y="30" width="10" height="50" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   
+   <rect x="350" y="0" width="10" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="390" y="0" width="10" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="360" y="0" width="30" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="370" y="30" width="10" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   
+   <rect x="410" y="0" width="10" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="410" y="90" width="40" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   
+   <rect x="460" y="0" width="10" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+   <rect x="460" y="90" width="40" height="10" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+</svg>
+    
+    </div>`
+}
 
 // used to check if every value in othersAreMutedArr if true
 const isTrue = (currentValue) => currentValue === true;
@@ -48,7 +103,6 @@ const handleJumpball = (event) => {
     console.log("you've got the jumpball")
     document.querySelector('.U26fgb.JRY2Pb').click()
     jumpballSquare.style.display = "none" 
-
 } 
 
 const callbackYouAreMuted = function(mutationsList) {
@@ -88,33 +142,49 @@ const observerOthersAreMuted = new MutationObserver(callbackOthersAreMuted);
 // checking if user joined meeting
 let intervalToCheckIfJoined = setInterval(() => {
     const checkIfJoinedElement = document.querySelector(".Jrb8ue")
+    const toAttachJumpballText = document.querySelector(".koV58.Zi94Db.S7urwe")
     if (!!checkIfJoinedElement) {
         console.log("joined meeting!")
-        clearInterval(intervalToCheckIfJoined)
+        if (!!toAttachJumpballText) {
+            attachJumpballText()
+            clearInterval(intervalToCheckIfJoined)
+        }
         // can turn off mic before user joins meeting
         // document.querySelector('.U26fgb.JRY2Pb').click()
     }
 
-    let intervalToCheckIfEveryoneIsMuted = setInterval(startAndStopJumpball, 1000);
+    let intervalToCheckIfEveryoneIsMuted = setInterval(() => {
+        let jumpballDisplayed;
+    // function startAndStopJumpball() {
+        // if (!!jumpballBool && youAreMutedBool === true && othersAreMutedArr.every(isTrue) && othersAreMutedArr.length!==0) {
 
-    function startAndStopJumpball() {
-        if (youAreMutedBool === true && othersAreMutedArr.every(isTrue) && othersAreMutedArr.length!==0) {
+        if (!jumpballDisplayed && youAreMutedBool === true && othersAreMutedArr.every(isTrue) && othersAreMutedArr.length!==0) {
             attachListenerToStartGameButton()
-            clearInterval(intervalToCheckIfEveryoneIsMuted);
+            // clearInterval(intervalToCheckIfEveryoneIsMuted);
+            jumpballDisplayed = true
             // here keeps on adding jumpballs
+            console.log('BBBBBBBBjumpballDisplayed', jumpballDisplayed)
+            // continue
         } 
         if (youAreMutedBool !== true || !othersAreMutedArr.every(isTrue)) { 
             const jumpballSquare = document.querySelector('.square')
             if (!!jumpballSquare) {
                 jumpballSquare.style.display = "none" 
             }
+            jumpballDisplayed = false
+            console.log('JJJJJJjumpballDisplayed', jumpballDisplayed)
+            // clearInterval(intervalToCheckIfEveryoneIsMuted);
         }
         
-    }
+    }, 5000);
+    
+    // startAndStopJumpball, 1000);
+    // add boolean
+    
 
     addObserverIfDesiredNodeAvailableYouAreMuted();
-    addObserverIfDesiredNodeAvailable();
-}, 1000)
+    addObserverIfDesiredNodeAvailableOthersAreMuted();
+}, 5000)
 
 function addObserverIfDesiredNodeAvailableYouAreMuted() {
     const youAreMuted = document.querySelector('.JHK7jb.hVsWVc.FTMc0c')
@@ -125,11 +195,11 @@ function addObserverIfDesiredNodeAvailableYouAreMuted() {
     observerYouAreMuted.observe(youAreMuted, config) 
 }
 
-function addObserverIfDesiredNodeAvailable() {
+function addObserverIfDesiredNodeAvailableOthersAreMuted() {
     const targetNode2 = document.querySelector('.lMHHZ')
     const secondTargetNodeList = document.querySelectorAll('.lMHHZ')
     if (!targetNode2) {
-        window.setTimeout(addObserverIfDesiredNodeAvailable,500);
+        window.setTimeout(addObserverIfDesiredNodeAvailableOthersAreMuted,500);
         return;
     }
 //? where does this for loop need to be?
